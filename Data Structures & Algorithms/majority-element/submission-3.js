85ab1bc4e50ce13d67f70811257b6feb94b54663
Aditya Yadav <44990373+aditya-yadav08@users.noreply.github.com>
2026-05-02 07:17:01 +0530

    Add: majority-element - submission-3

---
+++ Data Structures & Algorithms/majority-element/submission-3.js
@@ -0,0 +1,23 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    majorityElement(nums) {
+        let n = nums.length
+
+        for(let i=0; i<n; i++){
+            let count = 0
+
+            for(let j=0; j<n; j++){
+                if(nums[i] === nums[j]){
+                    count++
+                }
+            }
+
+            if(count >= Math.floor(n/2)){
+                return nums[i]
+            }
+        }
+    }
+}
